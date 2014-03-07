f39d28df9d0f60eedeae3638dc102a3d9e1c4023
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-07 11:27:59 +0000

    minor updates

--- lib/array.rb
+++ lib/array.rb
@@ -4,7 +4,7 @@
 # the result becomes the new value for memo. At the end of the iteration, 
 # the final value of memo is the return value for the method.
 class Array
-	
+  			
 	def infiltrate(memo = self.first)
 		if memo == self.first
 			self[1..-1].each do |element|

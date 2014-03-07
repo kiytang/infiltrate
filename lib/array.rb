861798f7c5b2d99c63d98148149dbd8491ddcd78
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-07 10:52:31 +0000

    all tests passing

--- lib/array.rb
+++ lib/array.rb
@@ -1,5 +1,20 @@
+# Notes from ruby-doc:
+# If you specify a block, then for each element in enum the block is 
+# passed an accumulator value (memo) and the element. 
+# the result becomes the new value for memo. At the end of the iteration, 
+# the final value of memo is the return value for the method.
 class Array
-	def infiltrate
-		
+	
+	def infiltrate(memo = self.first)
+		if memo == self.first
+			self[1..-1].each do |element|
+			memo = yield(memo, element)
+			end
+		else
+			self[0..-1].each do |element|
+			memo = yield(memo, element)
+		end
+	end	
+		memo			
 	end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -8,5 +8,53 @@ describe Array do
   		expect([1,2,3,4].infiltrate {|memo, number| memo + number}).to eq 10
   	end
 
+  	it 'can subtract all the elments from one another in an array of numbers' do
+  		expect([20,4,5,6].infiltrate {|memo, number| memo - number}).to eq 5
+  	end
+  	
+  	it 'can multiply all the elements in an array of numbers' do
+  		expect([1,2,3,4].infiltrate {|memo, number| memo * number}).to eq 24
+  	end 
+  	
+  	it 'can divide all the elements in an array of numbers' do
+  		expect([1000,100,10,1].infiltrate {|memo, number| memo / number}).to eq 1 
+  	end
+
+  	it 'can take a symbol (:+) as an argument instead of a block' do
+  		expect([1,2,3,4].infiltrate(&:+)).to eq 10
+  	end
+
+  	it 'can take a symbol (:-) as an argument instead of a block' do
+			expect([100,50,10,5].infiltrate(&:-)).to eq 35		
+  	end
+
+  	it 'can take a symbol (:*) as an argument instead of a block' do
+  		expect([1,2,3,4].infiltrate(&:*)).to eq 24
+  	end
+
+  	it 'can take a symbol (:/) as an argument instead of a block' do
+  		expect([100,2,2,5].infiltrate(&:/)).to eq 5
+  	end
+  	
+  	it 'can take a default value' do
+			expect([1,2,3,4].infiltrate(10) {|memo, number| memo + number}).to eq 20  		
+  	end
+
+  	it 'can add together a string of words' do
+  		expect(["the", "quick", "brown", "fox"].infiltrate {|memo, word| memo + word}).to eq "thequickbrownfox" 
+  	end
+
+  	it 'can add together a string of wrods with a default word to start' do
+  		expect(["world"].infiltrate("Hello") {|memo, word| memo + word}).to eq "Helloworld"
+  	end
+  	
+  	it 'can find the longest word from a collection of words' do
+  		expect(longest = %w{ cat sheep bear }.inject do |memo, word|
+   		memo.length > word.length ? memo : word end).to eq "sheep"	
+  	end
   end
  end
+
+		
+  	# take default values both words and numbers
+  	# add string of words with a default wordsreturn longest word from a collection of wordstring words together
